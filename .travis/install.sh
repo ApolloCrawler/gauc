d00a1a6ec4b6eb615e74e908a955e781a4618dfb
Tomas Korcak <korczis@gmail.com>
2016-10-11 17:04:26 +0200

    Install libcouchase

---
+++ .travis/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  brew update # get list of latest packages
+  brew install libcouchbase
+else
+  # Only needed during first-time setup:
+  wget http://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-2-amd64.deb
+  sudo dpkg -i couchbase-release-1.0-2-amd64.deb
+
+  # Will install or upgrade packages
+  sudo apt-get update
+  sudo apt-get install libcouchbase-dev libcouchbase2-bin build-essential libstdc++6 libc6 g++ llvm clang runit wget python-httplib2
+fi
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # gauc
 
 Rust Bindings for Couchbase
+
+## Status
+
+[![Build Status](https://travis-ci.org/korczis/gauc.svg?branch=master)](https://travis-ci.org/korczis/gauc)
+
