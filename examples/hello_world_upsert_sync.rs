dc558e8987592577bb398ed9579430cb5670c307
Tomas Korcak <korczis@gmail.com>
2016-10-24 00:32:25 +0200

    Sync upsert example

---
+++ examples/hello_world_upsert_sync.rs
@@ -0,0 +1,16 @@
+extern crate gauc;
+extern crate env_logger;
+
+use gauc::client::*;
+
+fn main() {
+    env_logger::init().unwrap();
+
+    let mut client = Client::new("couchbase://korczis.com/default");
+
+    let res = client.upsert_sync("hello_world_upsert_sync", "{{\"msg\": \"This is sync upsert!\"}}");
+    println!("{:?}", res);
+
+    let res = client.get_sync("hello_world_upsert_sync");
+    println!("{:?}", res);
+}
